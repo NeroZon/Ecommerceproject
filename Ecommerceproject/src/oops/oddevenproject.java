2e7ff1272f3efc779b66b4f6b5d019fe8cfc64a4
Arohi <Arohi@LAPTOP-TAU72GUO>
2023-02-10 07:42:26 +0530

    first commit

---
+++ Ecommerceproject/src/oops/oddevenproject.java
@@ -0,0 +1,21 @@
+package oops;
+
+public class oddevenproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=20 ;
+		
+		if (i%2==0) {
+		
+		System.out.println("your number is even");
+		}
+
+		else {
+	System.out.println("your number is odd");
+	
+	}
+	}
+
+}
